108c3b9e828b7af52b6b8ae0a874eb8527b59a4c
skarunan <skarunan@d3compute08.mpi-inf.mpg.de>
2019-11-13 10:04:16 +0100

    removing old files

--- src/regionQuery/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegionqueryConfig(AppConfig):
-    name = 'regionQuery'
--- src/table_manager/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TableManagerConfig(AppConfig):
-    name = 'table_manager'
